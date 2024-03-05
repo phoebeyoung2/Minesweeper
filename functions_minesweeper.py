7ffb7efe17ab533c3335b38448d1c8410b300727
phoebeyoung2 <162377300+phoebeyoung2@users.noreply.github.com>
2024-03-06 10:29:30 +1300

    Add files via upload

---
+++ functions_minesweeper.py
@@ -0,0 +1,102 @@
+def initialise_board():
+    board = ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+             'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']
+    return board
+
+
+def display_board(board):
+    i = 0
+    while i in range(len(board)):
+        print(board[i:i+5])
+        i = i + 5
+    return
+
+
+def insert_mines(board, positions):
+
+    for i in range(len(positions)):
+        col = positions[i][1]
+        row = positions[i][0]
+        mine_position = (5*row) + col
+        board[mine_position] = 'X'
+    return board
+
+
+def count_adjacent_mines(board, row, col):
+    counter = 0
+    mine_position = (5*row) + col
+
+    if col < 4 and board[mine_position+1] == 'X':
+        counter += 1
+
+    if col > 0 and board[mine_position-1] == 'X':
+        counter += 1
+
+    if row > 0 and board[mine_position-5] == 'X':
+        counter += 1
+
+    if row < 4 and board[mine_position+5] == 'X':
+        counter += 1
+
+    return counter
+
+
+def play_turn(board, row, col):
+    counter = count_adjacent_mines(board, row, col)
+    mine_position = (5*row) + col
+
+    if board[mine_position] == 'X':
+        board[mine_position] = '#'
+    elif counter > 0 and board[mine_position] == 'O':
+        board[mine_position] = counter
+    elif counter == 0 and board[mine_position] == 'O':
+        board[mine_position] = ' '
+
+    return board[mine_position]
+
+
+def check_win(board):
+    i = 0
+    position_counter = 0
+
+    hidden_mines = 0
+    found_mines = 0
+
+    for i in range(len(board)):
+        if board[i] == 'O':
+            position_counter += 1
+        if board[i] == 'X':
+            hidden_mines += 1
+        if board[i] == '#':
+            found_mines += 1
+    i += 1
+
+    if position_counter == 0 and found_mines == 0:
+        return bool(1)
+
+    if position_counter > 0 or found_mines < hidden_mines:
+        return bool(0)
+
+
+def play_game(positions):
+    initial_board = initialise_board()
+    hidden_mines_board = initialise_board()
+    non_hidden_mines_board = insert_mines(initial_board, positions)
+    display_board(hidden_mines_board)
+
+    while not check_win(non_hidden_mines_board):
+
+        row = int(input("Enter row number (1-5):"))
+        col = int(input("Enter column number (1-5):"))
+        row -= 1
+        col -= 1
+
+        mine_position = (5*row) + col
+
+        play_turn(non_hidden_mines_board, row, col)
+        hidden_mines_board[mine_position] = non_hidden_mines_board[mine_position]
+        display_board(hidden_mines_board)
+
+    if check_win(non_hidden_mines_board) == bool(1):
+        print("You won!")
+        return
---
+++ task1.py
@@ -0,0 +1,4 @@
+from functions_minesweeper import *
+play_game([[2, 3], [1, 4]])
+
+
